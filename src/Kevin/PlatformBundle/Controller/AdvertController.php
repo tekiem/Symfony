97ea632defdcb5ba34d6b50d2d0b8ddd263f8689
Kevin <kevdeplc@hotmail.fr>
2018-06-22 02:22:06 +0200

    quatre

--- src/Kevin/PlatformBundle/Controller/AdvertController.php
+++ src/Kevin/PlatformBundle/Controller/AdvertController.php
@@ -7,6 +7,9 @@ namespace Kevin\PlatformBundle\Controller;
 // N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse; // N'oubliez pas ce use
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdvertController extends Controller
