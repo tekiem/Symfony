9c9f816975916f56df5d2c49150a98f031db34cf
Kevin <kevdeplc@hotmail.fr>
2018-08-20 22:27:12 +0200

    ajout sql 2

--- symfony.sql
+++ symfony.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 27 juil. 2018 à 12:22
+-- Généré le :  lun. 20 août 2018 à 20:26
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.2.4
 
@@ -41,17 +41,18 @@ CREATE TABLE IF NOT EXISTS `advert` (
   `nb_applications` int(11) NOT NULL,
   `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_54F1F40B989D9B62` (`slug`),
   UNIQUE KEY `UNIQ_54F1F40B2B36786B` (`title`),
+  UNIQUE KEY `UNIQ_54F1F40B989D9B62` (`slug`),
   UNIQUE KEY `UNIQ_54F1F40B3DA5256D` (`image_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `advert`
 --
 
 INSERT INTO `advert` (`id`, `image_id`, `date`, `title`, `author`, `content`, `published`, `updated_at`, `nb_applications`, `slug`) VALUES
-(7, 8, '2018-07-27 11:54:00', 'Ronaldinho deee', 'Kevin', 'Kevin', 1, NULL, 0, 'ronaldinho-deee');
+(1, 1, '2018-08-07 17:43:00', 'Je m\'appelle Kevin', 'Kevin', 'Ici c\'est Paris', 1, NULL, 0, 'je-mappelle-kevin'),
+(2, 2, '2018-08-20 20:07:00', 'Je m\'appelle Kevin2', 'fjgfg', '<p>fgjhfgj</p>', 1, '2018-08-20 20:11:22', 0, 'je-mappelle-kevin2');
 
 -- --------------------------------------------------------
 
@@ -73,7 +74,8 @@ CREATE TABLE IF NOT EXISTS `advert_category` (
 --
 
 INSERT INTO `advert_category` (`advert_id`, `category_id`) VALUES
-(7, 16);
+(1, 11),
+(2, 11);
 
 -- --------------------------------------------------------
 
@@ -120,18 +122,18 @@ CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `category`
 --
 
 INSERT INTO `category` (`id`, `name`) VALUES
-(16, 'Développement web'),
-(17, 'Développement mobile'),
-(18, 'Graphisme'),
-(19, 'Intégration'),
-(20, 'Réseau');
+(11, 'Développement web'),
+(12, 'Développement mobile'),
+(13, 'Graphisme'),
+(14, 'Intégration'),
+(15, 'Réseau');
 
 -- --------------------------------------------------------
 
@@ -145,14 +147,15 @@ CREATE TABLE IF NOT EXISTS `image` (
   `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `alt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `image`
 --
 
 INSERT INTO `image` (`id`, `url`, `alt`) VALUES
-(8, 'jpeg', 'roni.jpg');
+(1, 'jpeg', 'téléchargement.jpg'),
+(2, 'jpeg', 'téléchargement.jpg');
 
 -- --------------------------------------------------------
 
@@ -165,20 +168,20 @@ CREATE TABLE IF NOT EXISTS `skill` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `skill`
 --
 
 INSERT INTO `skill` (`id`, `name`) VALUES
-(22, 'PHP'),
-(23, 'Symfony'),
-(24, 'C++'),
-(25, 'Java'),
-(26, 'Photoshop'),
-(27, 'Blender'),
-(28, 'Bloc-note');
+(15, 'PHP'),
+(16, 'Symfony'),
+(17, 'C++'),
+(18, 'Java'),
+(19, 'Photoshop'),
+(20, 'Blender'),
+(21, 'Bloc-note');
 
 -- --------------------------------------------------------
 
@@ -190,28 +193,32 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
   `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
-  `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email_canonical` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
   `enabled` tinyint(1) NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_login` datetime DEFAULT NULL,
   `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password_requested_at` datetime DEFAULT NULL,
+  `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_8D93D64992FC23A8` (`username_canonical`),
   UNIQUE KEY `UNIQ_8D93D649A0D96FBF` (`email_canonical`),
   UNIQUE KEY `UNIQ_8D93D649C05FB297` (`confirmation_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `salt`, `roles`, `username_canonical`, `email`, `email_canonical`, `enabled`, `last_login`, `confirmation_token`, `password_requested_at`) VALUES
-(11, 'kevin', 'pE8QZ9JbsMyANkr9KPk4bMhNf0oj8vfjWG3wOwxUXYMlOq8iU6uoFPP4JsXgYvyqhfuC88gykyLsSKqRU5AZlw==', 'VMdZS0UwCikV4YPbW2cS7MLB.EIp2aR8V2k9.A/qhtM', 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}', 'kevin', 'kevdeplc@hotmail.fr', 'kevdeplc@hotmail.fr', 1, '2018-07-27 12:18:44', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`) VALUES
+(1, 'Alexandre', 'alexandre', NULL, NULL, 0, '', 'Alexandre', NULL, NULL, NULL, 'a:0:{}'),
+(2, 'Marine', 'marine', NULL, NULL, 0, '', 'Marine', NULL, NULL, NULL, 'a:0:{}'),
+(3, 'Anna', 'anna', NULL, NULL, 0, '', 'Anna', NULL, NULL, NULL, 'a:0:{}'),
+(4, 'kevin', 'kevin', NULL, NULL, 0, '', 'kevin', NULL, NULL, NULL, 'a:0:{}'),
+(5, 'kevinplc', 'kevinplc', 'kevin.huor@hotmail.fr', 'kevin.huor@hotmail.fr', 1, '3.wy..EQfK9fEZo.RAWF2LHRIy3eefLAvuuAgf7QVLQ', 'O3PDir0ElrRh+zU03Z98bgGqkS1QKPIViu1zbOtOHe1K3eh24aFFsYHYujP2oIn3h6IVMEa9FVYCefDKnNzMSQ==', '2018-08-20 13:21:21', NULL, NULL, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}');
 
 --
 -- Contraintes pour les tables déchargées
